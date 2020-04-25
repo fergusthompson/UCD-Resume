0b427cf8df5f2b56097a614733ef6826f58ea0b0
fergusthompson <fergus.thompson@hotmail.com>
2020-04-25 14:56:05 +0000

    added emailjs

--- js/github-information.js
+++ js/github-information.js
@@ -34,6 +34,8 @@ function repoInformationHTML(repos){
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a github username</h2>`);
@@ -54,9 +56,15 @@ function fetchGitHubInformation(event) {
         }, function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`)
-            } else {
-                console.log(errorRespons);
+            } else if(errorResponse.status ===403 ){
+                var resetTime = new Date(errorResponse.getResponseHeader(`x-RateLimit-Reset`)*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            }else{
+            
+                console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
